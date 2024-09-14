c01a26fe051572fa4bd819247549694118dae580
Enrico Deleo <hello@enricodeleo.com>
2024-09-14 20:59:04 +0200

    feat(api/controllers/users/list.js): implement database integration for user listing functionality

--- api/controllers/users/list.js
+++ api/controllers/users/list.js
@@ -1,4 +1,9 @@
-export default async function (ctx) {
-  // Handle listing users
-  return 'Listing users...';
+export default async function listUsers(ctx) {
+  const userRepository = cambusa.db.getRepository('User');
+
+  // Fetch all users from the database
+  const users = await userRepository.find();
+
+  // Return the list of users
+  return users;
 }
