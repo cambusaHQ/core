6eeb464be5e291f3e5a9b45da8f99af5aad20884
Enrico Deleo <hello@enricodeleo.com>
2024-09-18 08:54:35 +0200

    wip: sqlite drivers

--- api/controllers/users/create.js
+++ api/controllers/users/create.js
@@ -1,5 +1,16 @@
 export default async function createUser(ctx) {
+  // Hardcoding user data for testing purposes
+  const hardcodedUser = {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com'
+  };
   const userRepository = cambusa.db.getRepository('User');
-  const newUser = await userRepository.save(ctx.body);
+  console.log(cambusa.models);
+  const user = new cambusa.models.User();
+  user.firstName = 'Enrico';
+  user.lastName = 'Deleo';
+  user.email = 'enricodeleo@gmail.com';
+  const newUser = await userRepository.save(user);
   return newUser;
 }
--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 export default {
   database: {
-    type: 'sqlite', // Change this to one of the supported databases in TypeORM
+    type: 'better-sqlite3', // Change this to one of the supported databases in TypeORM
     url: './database.sqlite',
   },
 };
